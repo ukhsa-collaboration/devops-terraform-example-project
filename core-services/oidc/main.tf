07a7928f423914ec6129da943ab5de5b700f1a8f
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-09-25 13:40:04 +0100

    DOE-201 Correct location of Terraform module (#42)

--- core-services/oidc/main.tf
+++ core-services/oidc/main.tf
@@ -1,5 +1,5 @@
 module "aws-oidc-github" {
-  source = "git::ssh://git@github.com/UKHSA-Internal/devops-terraform-modules.git?ref=8e2891e661b95fe30d9bd3b42ccc80e4c356f16a"
+  source = "git::ssh://git@github.com/UKHSA-Internal/devops-terraform-modules//terraform-modules/aws/oidc?ref=8e2891e661b95fe30d9bd3b42ccc80e4c356f16a"
 
   repo_name = "UKHSA-Internal/devops-terraform-example-project"
   additional_allowed_repos = {

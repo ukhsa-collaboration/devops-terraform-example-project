93ed41e6321b3932ea052b9e799d457fd2f21cd4
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2025-01-29 10:42:02 +0000

    Fix permissions

--- core-services/oidc/main.tf
+++ core-services/oidc/main.tf
@@ -17,40 +17,6 @@ module "iam_github_oidc_role" {
   ]
 
   policies = {
-    CIUser = aws_iam_policy.ci_user.arn
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
   }
-}
-
-resource "aws_iam_policy" "ci_user" {
-  name = "github-actions-oidc"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "dynamodb:*",
-          "ecr:*",
-          "s3:*",
-          "servicediscovery:*",
-          "application-autoscaling:*",
-          "logs:*",
-          "elasticache:*",
-          "elasticloadbalancing:*",
-          "ecs:*",
-          "ec2:*",
-          "kms:*"
-        ],
-        "Resource" : [
-          "*"
-        ],
-        "Condition" : {
-          "StringEquals" : {
-            "aws:RequestedRegion" : ["eu-west-2"]
-          }
-        }
-      }
-    ]
-  })
 }
